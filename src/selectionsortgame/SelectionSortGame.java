9b156391a501b6300498c7af772e5ac1df536993
JayZYI <zayidanilham7@gmail.com>
2023-01-06 13:53:31 +0700

    first commit

---
+++ src/selectionsortgame/SelectionSortGame.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package selectionsortgame;
+
+import main.UserFrame;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SelectionSortGame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        UserFrame user = new UserFrame();
+        user.setLocationRelativeTo(null);
+        user.setVisible(true);
+    }
+    
+}
